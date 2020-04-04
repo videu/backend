9090da76755e032684e54ba0e4c2c99c6465c7bd
Felix Kopp <sandtler@sandtler.club>
2020-04-04 21:09:37 +0200

    Add missing type

---
+++ types/json/response.ts
@@ -0,0 +1,116 @@
+/**
+ * @file General HTTP response interface definitions.
+ * @author Felix Kopp <sandtler@sandtler.club>
+ *
+ * @license
+ * Copyright (c) 2020 The videu Project <videu@freetube.eu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/** An enumeration of all HTTP status codes. */
+export const enum HTTPStatusCode {
+
+    /* 2xx success */
+
+    OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+    NON_AUTHORITATIVE_INFORMATION = 203,
+    NO_CONTENT = 204,
+    RESET_CONTENT = 205,
+    PARTIAL_CONTENT = 206,
+
+    /* 3xx redirects */
+
+    MULTIPLE_CHOICES = 300,
+    MOVED_PERMANENTLY = 301,
+    FOUND = 302,
+    SEE_OTHER = 303,
+    NOT_MODIFIED = 304,
+    USE_PROXY = 305,
+    TEMPORARY_REDIRECT = 307,
+    PERMANENT_REDIRECT = 308,
+
+    /* 4xx client errors */
+
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    PAYMENT_REQUIRED = 402,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+    NOT_ACCEPTABLE = 406,
+    PROXY_AUTHENTICATION_REQUIRED = 407,
+    REQUEST_TIMEOUT = 408,
+    CONFLICT = 409,
+    GONE = 410,
+    LENGTH_REQUIRED = 411,
+    PRECONDITION_FAILED = 412,
+    REQUEST_ENTITY_TOO_LARGE = 413,
+    URI_TOO_LONG = 414,
+    UNSUPPORTED_MEDIA_TYPE = 415,
+    REQUESTED_RANGE_NOT_SATISFIABLE = 416,
+    EXPECTATION_FAILED = 417,
+    BLAZE_IT = 420,
+    MISDIRECTED_REQUEST = 421,
+    UNPROCESSABLE_ENTITY = 422,
+    LOCKED = 423,
+    FAILED_DEPENDENCY = 424,
+    UPGRADE_REQUIRED = 426,
+    PRECONDITION_REQUIRED = 428,
+    TOO_MANY_REQUESTS = 429,
+    UNAVAILABLE_FOR_LEAGL_REASONS = 451,
+
+    /* 5xx server errors */
+
+    INTERNAL_SERVER_ERROR = 500,
+    NOT_IMPLEMENTED = 501,
+    BAD_GATEWAY = 502,
+    SERVICE_UNAVAILABLE = 503,
+    GATEWAY_TIMEOUT = 504,
+    HTTP_VERSION_NOT_SUPPORTED = 505,
+    VARIANT_ALSO_NEGOTIATES = 506,
+    INSUFFICIENT_STORAGE = 507,
+    LOOP_DETECTED = 508,
+    NOT_EXTENDED = 510,
+    NETWORK_AUTHENTICATION_REQUIRED = 511,
+
+}
+
+/**
+ * An HTTP response body containing an error message.
+ * The corresponding HTTP status code MAY be an error code, but this is not
+ * strictly required.
+ */
+export interface IErrorResponseBody {
+
+    err: true;
+
+    /** The (human-readable) error message to display on the client. */
+    msg: string;
+
+}
+
+/**
+ * A successful HTTP response body.
+ */
+export interface ISuccessResponseBody {
+
+    err: false;
+
+}
+
+/** Either a success or error response in JSON format. */
+export type JSONResponseBody = IErrorResponseBody | ISuccessResponseBody;
